316ec35db8ebab8341271277fc32858154eb9d10
Sidiq Munandar <sidiqveteran123@gmail.com>
2025-11-12 01:03:59 +0700

    Remove unused React import

--- personachain-frontend/src/components/AssessmentForm.tsx
+++ personachain-frontend/src/components/AssessmentForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { saveMintedNFT } from '@/utils/storage';
 import { generateNFTMetadata } from '@/utils/nftGenerator';
