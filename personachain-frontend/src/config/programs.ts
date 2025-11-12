7e679fd48184d5d47ec809069670266b13505ded
Sidiq Munandar <sidiqveteran123@gmail.com>
2025-11-12 17:50:07 +0700

    Add config/programs.ts

---
+++ personachain-frontend/src/config/programs.ts
@@ -0,0 +1,3 @@
+export const PERSONA_NFT_PROGRAM_ID = '9xTVGjPzXhhUsQnD2obZgXmTYvFRePvfZyn8EZPM568X';
+export const CARV_RPC = 'https://rpc.testnet.carv.io/rpc';
+export const CARV_NETWORK = 'https://rpc.testnet.carv.io/rpc';
